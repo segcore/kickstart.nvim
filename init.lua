c8872776330da2505f8b29b68adb34c7f8d886bf
segcore <44293604+segcore@users.noreply.github.com>
2023-11-28 22:33:55 +0800

    lualine: include relative path, searchcount and filesize

--- init.lua
+++ init.lua
@@ -153,7 +153,6 @@ require('lazy').setup({
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000,
     config = function()
       vim.cmd.colorscheme 'catppuccin-latte'
     end
@@ -169,6 +168,12 @@ require('lazy').setup({
         theme = 'auto',
         component_separators = '|',
         section_separators = '',
+      },
+      sections = {
+        lualine_c = {
+          { 'filename', path = 1 },
+        },
+        lualine_x = { 'searchcount', 'filesize', 'encoding', 'fileformat', 'filetype' },
       },
     },
     dependencies = {
@@ -476,7 +481,7 @@ require('which-key').register {
   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
   ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
+  ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
