ab200cd9a8201cabea3e06c9d7579920608cd2f1
segcore <44293604+segcore@users.noreply.github.com>
2024-05-17 16:23:39 +0800

    Add undotree

--- init.lua
+++ init.lua
@@ -856,6 +856,17 @@ require('lazy').setup({
     'tpope/vim-dispatch',
   },
 
+  {
+    'mbbill/undotree',
+    event = 'VeryLazy',
+    config = function()
+      vim.g.undotree_WindowLayout = 3 -- right side
+      vim.g.undotree_SetFocusWhenToggle = 1
+      vim.g.undotree_SplitWidth = 40
+      vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    end,
+  },
+
   -- Vim practice with :VimBeGood
   'ThePrimeagen/vim-be-good',
 
