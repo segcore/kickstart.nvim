6bf3382eecd6835f60e5cb40125d6e6d8902032b
segcore <44293604+segcore@users.noreply.github.com>
2024-08-06 10:01:50 +0800

    Update which-key spec

--- init.lua
+++ init.lua
@@ -820,13 +820,14 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VeryLazy',
     config = function()
-      require('which-key').setup()
-      require('which-key').register {
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
+      require('which-key').setup({
+        spec = {
+          { "<leader>g",  group = "[G]it" },
+          { "<leader>h",  group = "[H]arpoon" },
+          { "<leader>s",  group = "[S]earch" },
+          { "<leader>w",  group = "[W]orkspace" },
+        },
+      })
     end,
   },
 
