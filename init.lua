f42f086d617d47c845580b4e8e111d082b41d975
segcore <44293604+segcore@users.noreply.github.com>
2024-09-18 10:00:21 +0800

    Revert catppuccin's darker comment foreground

--- init.lua
+++ init.lua
@@ -308,9 +308,14 @@ require('lazy').setup({
       vim.cmd.colorscheme 'catppuccin-latte'
 
       vim.cmd([[
-      hi LspReferenceText  guibg=#afdcfa
-      hi LspReferenceRead  guibg=#92f7aa
-      hi LspReferenceWrite guibg=#88e39e
+        highlight LspReferenceText  guibg=#afdcfa
+        highlight LspReferenceRead  guibg=#92f7aa
+        highlight LspReferenceWrite guibg=#88e39e
+
+        " Revert catppuccin's Comment highlight change, to make comments lighter
+        " and easier to distinguish from code
+        " highlight Comment guifg=#7c7f93 " catppuccin's new colour
+        highlight Comment guifg=#9ca0b0   " catppuccin's old colour
       ]])
     end,
     opts = {
